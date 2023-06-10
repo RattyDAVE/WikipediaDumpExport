acea1038b69e64bd727699c479bbf0b30dc5c3c3
RattyDAVE <dave@pucknell.co.uk>
2023-06-10 13:10:03 +0100

    Update dump.py

--- dump.py
+++ dump.py
@@ -84,7 +84,8 @@ def process_file_text(filename, savedir):
             elif '</page>' in line:  # end of article
                 #Thread(target=save_article, args=(article, savedir)).start()
                 
-                with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+                #with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+                with concurrent.futures.ThreadPoolExecutor() as executor:
                     executor.submit(save_article, article, savedir)
                 
                 #save_article(article, savedir)
