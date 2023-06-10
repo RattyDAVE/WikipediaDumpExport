608cf8ab94d2fad453d1571bb0f6b22112f49aec
RattyDAVE <dave@pucknell.co.uk>
2023-06-10 10:27:54 +0100

    Create dump.py

---
+++ dump.py
@@ -0,0 +1,102 @@
+from threading import Thread
+import json
+import re
+from html2text import html2text as htt
+import wikitextparser as wtp
+import unicodedata
+
+import bz2
+import urllib.request
+import os
+
+def dewiki(text):
+    text = wtp.parse(text).plain_text()  # wiki to plaintext 
+    text = htt(text)  # remove any HTML
+    #text = text.replace('\\n',' ')  # replace newlines
+    #text = re.sub('\s+', ' ', text)  # replace excess whitespace
+    return text
+
+
+def analyze_chunk(text):
+    try:
+        if '<redirect title="' in text:  # this is not the main article
+            return None
+        if '(disambiguation)' in text:  # this is not an article
+            return None
+        else:
+            title = text.split('<title>')[1].split('</title>')[0]
+            title = htt(title)
+            if ':' in title:  # most articles with : in them are not articles we care about
+                return None
+        serial = text.split('<id>')[1].split('</id>')[0]
+        content = text.split('</text')[0].split('<text')[1].split('>', maxsplit=1)[1]
+        content = dewiki(content)
+        return {'title': title.strip(), 'text': content.strip(), 'id': serial.strip()}
+    except Exception as oops:
+        print(oops)
+        return None
+
+def slugify(value, allow_unicode=False):
+    """
+    For cleaning up file names
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+def save_article(article, savedir):
+    doc = analyze_chunk(article)
+    if doc:
+        print('SAVING:', doc['title'])
+        #filename = doc['id'] + '.json'
+        filename = slugify(doc['title'])  + '.txt'
+        with open(savedir + filename, 'w', encoding='utf-8') as outfile:
+            #json.dump(doc, outfile, sort_keys=True, indent=1, ensure_ascii=False)
+            outfile.write(doc['text'])
+
+
+def process_file_text(filename, savedir):
+    article = ''
+    #with open(filename, 'r', encoding='utf-8') as infile:
+    with bz2.open(filename, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            if '<page>' in line:
+                article = ''
+            elif '</page>' in line:  # end of article
+                #Thread(target=save_article, args=(article, savedir)).start()
+                save_article(article, savedir)
+                #exit()
+            else:
+                article += line
+                
+
+####### START HERE ######
+
+#wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
+#wiki_xml_file = '/share/simplewiki-latest-pages-articles.xml'  # update this
+wiki_xml_file = '/share/simplewiki-latest-pages-articles.xml.bz2'  # update this
+json_save_dir = '/share/out/'
+
+if __name__ == '__main__':
+
+    
+    url = "https://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pages-articles.xml.bz2"
+    filename = "simplewiki-latest-pages-articles.xml.bz2"
+
+    if os.path.exists(filename):
+        print(f"The file '{filename}' exists.")
+    else:
+        print(f"The file '{filename}' does not exist. Downloading")
+        urllib.request.urlretrieve(url, filename)
+        print("Download complete!")                
+
+    #wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
+    #wiki_xml_file = '/share/simplewiki-latest-pages-articles.xml'  # update this
+    wiki_xml_file = '/share/simplewiki-latest-pages-articles.xml.bz2'  # update this
+    json_save_dir = '/share/out/'
+ 
+    process_file_text(wiki_xml_file, json_save_dir)
